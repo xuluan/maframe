0940b784c6fdfce0153a16d1e8d04131f8d643de
xuluan <rolle.xu@gmail.com>
2013-01-05 16:11:33 +0800

    update project dir

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 maframe
 =======
 
-maframe
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,26 @@
+
+
 class Project < ActiveRecord::Base
+  PRJ_HOME = "prj"
+
   attr_accessible :name, :path
+
+  before_create :dir_create
+
+  before_destroy :dir_destroy
+
+
+  def dir_create
+    Dir.mkdir(PRJ_HOME) unless Dir.exist?(PRJ_HOME)
+    prj_dir = "#{PRJ_HOME}/#{self.name}"
+    #TODO dir exist or mkdir fail
+    Dir.mkdir(prj_dir)
+    self.path = File.absolute_path(prj_dir)
+  end
+
+  def dir_destroy
+
+    prj_dir = "#{PRJ_HOME}/#{self.name}"
+    system("rm #{prj_dir} -rf")    
+  end
 end
