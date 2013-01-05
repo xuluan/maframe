5c784b1247a2040870303cf9f9293035f4c3f091
xuluan <rolle.xu@gmail.com>
2013-01-05 13:25:20 +0800

    intial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maframe::Application.config.secret_token = '6e4b13bb94074be473a96cce5d4bcfbdd99b3646fcf57fc026fc41f423d45bfb943c497f1fda6028835114d590b6ab084b98bf222af41e14a0ff15a3f0785242'
